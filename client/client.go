acf02238f81f34afc45a8076fac118d1e25d97cd
PeterShen <petershen0307@hotmail.com>
2020-06-10 08:27:33 +0800

    refine pipe client

--- client/client.go
+++ client/client.go
@@ -7,8 +7,6 @@ import (
 	"log"
 	"net"
 	"os"
-	"os/signal"
-	"syscall"
 	"time"
 
 	"github.com/Microsoft/go-winio"
@@ -23,31 +21,22 @@ func main() {
 func pipeClient(pipeName string) {
 	fullPipeName := fmt.Sprintf(`\\.\pipe\%s`, pipeName)
 	log.Println(fullPipeName)
+
 	var conn net.Conn
 	var err error
-	go func() {
-		for {
-			conn, err = winio.DialPipe(fullPipeName, nil)
-			if err != nil {
-				log.Println("dial with error:", err, "sleep 1 second")
-				time.Sleep(1 * time.Second)
-				continue
-			} else {
-				break
-			}
+	for {
+		conn, err = winio.DialPipe(fullPipeName, nil)
+		if err != nil {
+			log.Println("dial with error:", err, "sleep 1 second")
+			time.Sleep(1 * time.Second)
+			continue
+		} else {
+			break
 		}
-		log.Println("start connection")
-		for scanner := bufio.NewScanner(os.Stdin); scanner.Scan(); {
-			fmt.Fprintln(conn, scanner.Text())
-		}
-	}()
-	c := make(chan os.Signal)
-	signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
-	defer signal.Stop(c)
-	<-c
-	log.Println("shutdown")
-	if conn != nil {
-		log.Println("close connection")
-		defer conn.Close()
 	}
+	log.Println("start connection")
+	for scanner := bufio.NewScanner(os.Stdin); scanner.Scan(); {
+		fmt.Fprintln(conn, scanner.Text())
+	}
+	defer conn.Close()
 }
